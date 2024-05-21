55ae0944c118423cd2dd32f033f2a3d893634f9c
T12 computer <damonc@dtu.dk>
2024-05-21 15:41:10 +0200

    add measurement_time function to station

--- qcodes/station.py
+++ qcodes/station.py
@@ -291,3 +291,10 @@ class Station(Metadatable, DelegateAttributes):
         return self.components[key]
 
     delegate_attr_dicts = ['components']
+
+    def measurement_time(self,averages=1):
+        start=time.time()
+        for repeats in range(averages):
+            self.measurement()
+        end=time.time()
+        print('Average measurement time='+str((end-start)/averages)+'s')
